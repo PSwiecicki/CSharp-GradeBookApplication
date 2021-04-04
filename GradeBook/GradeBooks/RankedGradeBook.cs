5ebc1eb182ce506c416aefe9f9a01181148ac4d4
Patryk Święickci <patswiecicki@gmail.com>
2021-04-04 20:14:22 +0200

    Add method GetLetterGrade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -10,5 +10,39 @@ namespace GradeBook.GradeBooks
         {
             Type = Enums.GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            char grade = 'F';
+
+            if (Students.Count > 5)
+            {
+                int place = Students.Count;
+                foreach(var student in Students)
+                {
+                    if (averageGrade > student.AverageGrade)
+                        place--;
+                }
+                double d = place / Students.Count;
+                if(d < 0.2)
+                {
+                    grade = 'A';
+                }
+                else if(d < 0.4)
+                {
+                    grade = 'B';
+                }
+                else if (d < 0.6)
+                {
+                    grade = 'C';
+                }
+                else if (d < 0.8)
+                {
+                    grade = 'D';
+                }
+            }
+            return grade;
+
+        }
     }
 }
