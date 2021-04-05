8c52a1c7d48bb77bc0708ea877e4b37eda638cdb
Patryk Święickci <patswiecicki@gmail.com>
2021-04-05 11:21:07 +0200

    Add weighted gradebook option

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -14,11 +14,13 @@ namespace GradeBook.GradeBooks
         public string Name { get; set; }
         public List<Student> Students { get; set; }
         public GradeBookType Type { get; set; }
+        public bool IsWeighted { get; set; }
 
-        public BaseGradeBook(string name)
+        public BaseGradeBook(string name, bool isWeighted)
         {
             Name = name;
             Students = new List<Student>();
+            IsWeighted = isWeighted;
         }
 
         public void AddStudent(Student student)
@@ -107,20 +109,30 @@ namespace GradeBook.GradeBooks
 
         public virtual double GetGPA(char letterGrade, StudentType studentType)
         {
+            int value = IsWeighted ? 1 : 0;
+
             switch (letterGrade)
             {
                 case 'A':
-                    return 4;
+                    value += 4;
+                    break;
                 case 'B':
-                    return 3;
+                    value += 3;
+                    break;
                 case 'C':
-                    return 2;
+                    value += 2;
+                    break;
                 case 'D':
-                    return 1;
+                    value += 1;
+                    break;
                 case 'F':
-                    return 0;
+                    value += 0;
+                    break;
+                default:
+                    value = 0;
+                    break;
             }
-            return 0;
+            return value;
         }
 
         public virtual void CalculateStatistics()
